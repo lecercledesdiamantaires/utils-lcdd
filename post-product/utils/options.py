31d4f37a03aca50a6c853cf76daf84feece0a4c4
Anthony <anthonylopes037@gmail.com>
2024-12-06 15:14:43 +0100

    remove loop

--- post-product/utils/options.py
+++ post-product/utils/options.py
@@ -1,7 +1,5 @@
-from get_infos import get_infos
 
-def create_options():
-    infos = get_infos()
+def create_options(type):
     colors= {
         "name" : "Couleur de l'or",
         "values" : ["Rose gold","Gold","White gold"]
@@ -11,20 +9,18 @@ def create_options():
         "values": ["14", "18"]
     }
     options = []
-    for info in infos:
-        if info['product_type'] == 'Bague':
-            size = {
-                "name" : "Taille",
-                "values" : ["48","49","50","51", "52","53", "54","55", "56", "57","58", "59","60","61","62","63"]
-            }
-            options.append(size)
-        elif info['product_type'] == 'Bracelet':
-            size = {
-                "name" : "Taille",
-                "values" : ["XXS","XS","S","M", "L","XL", "XXL"]
-            }
-            options.append(size)
-        options.append(colors)
-        options.append(carats)
-        print('produit' + info['product_type'])
+    if type == 'Bague':
+        size = {
+            "name" : "Taille",
+            "values" : ["48","49","50","51", "52","53", "54","55", "56", "57","58", "59","60","61","62","63"]
+        }
+        options.append(size)
+    elif type == 'Bracelet':
+        size = {
+            "name" : "Taille",
+            "values" : ["XXS","XS","S","M", "L","XL", "XXL"]
+        }
+        options.append(size)
+    options.append(colors)
+    options.append(carats)
     return options
