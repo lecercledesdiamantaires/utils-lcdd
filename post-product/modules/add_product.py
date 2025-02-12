3ee2563553263fd42f310bdbe7e7698c8cbe960f
cactusninjaa <thomas.sauvage@edu-devinci.fr>
2025-02-12 10:26:13 +0100

    UPDATE env

--- post-product/modules/add_product.py
+++ post-product/modules/add_product.py
@@ -7,12 +7,10 @@ API_KEY = st.secrets["API_KEY"]
 PASSWORD = st.secrets["PASSWORD"]
 SHOPIFY_STORE = st.secrets["SHOP_URL"]
 SHOPIFY_API_KEY = st.secrets["API_KEY"]
-SHOPIFU_API_PASSWORD = st.secrets["PASSWORD"]
 SHOP_NAME = st.secrets["SHOP_NAME"]
 API_VERSION = st.secrets["API_VERSION"]
 LIMIT = st.secrets["LIMIT"]
 FALSE_URL = st.secrets["FALSE_URL"]
-SHOPIFY_STORE_URL = st.secrets["SHOPIFY_STORE"]
 BASE_URL = f"https://{API_KEY}:{PASSWORD}@{SHOPIFY_STORE}"
 
 def add_product(data):
